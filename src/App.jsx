7edeffa13b1ebb3c3cf2bc9df22c7535c6f1c513
Daesy Vences <daesydesign@gmail.com>
2021-06-29 16:21:16 -0500

    updated with walkthrough

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Greeter from './components/Greeter';
 
 // function App() {
@@ -11,14 +11,33 @@ import Greeter from './components/Greeter';
 // export default App;
 
 let App = () => {
-  return (
-    <div>
-      <Greeter phrase="Hola" name="Daze" />
-      <Greeter phrase="Wimwega" name="Rico" />
-      <Greeter phrase="I AM" name="BATMAN" />
-      {/* <input */}
-    </div>
-  );
+  const [username, setUsername] = useState('');
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoaded(true);
+    }, 3000);
+  }, [loaded]);
+
+  if (loaded) {
+    return (
+      <>
+        <Greeter phrase="Hola" name="Daze" />
+        <Greeter phrase="Wimwega" name="Rico" />
+        <Greeter phrase="I AM" name="BATMAN" />
+        <input type="text" placeholder="Type your username" value={username} onChange={(e) => { setUsername(e.target.value) }} />
+        <p>You are logging in as {username}!</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h2>Pagina Loading...</h2>
+        <button onClick={() => setLoaded(true)}>Load Pagina</button>
+      </>
+    )
+  }
 };
 
 export default App;
--- src/components/Greeter.jsx
+++ src/components/Greeter.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 
-let Greeter = ({ phrase, name }) => {
+
+/******"normal" way*******/
+let Greeter = props => {
     return (
         <div>
-            <h1>{phrase}, {name}!</h1>
+            <h1>{props.phrase}, {props.name}!</h1>
         </div>
     )
 }
+
+
+
+/******destructured way*******/
+// let Greeter = ({ phrase, name }) => {
+//     return (
+//         <div>
+//             <h1>{phrase}, {name}!</h1>
+//         </div>
+//     )
+// }
 
 export default Greeter;
